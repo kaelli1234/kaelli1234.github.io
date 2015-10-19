a9083cb392b712e99909c0bc76fc0e1dd671bcca
Kael Li <kaelli@gmail.com>
2015-10-19 17:36:25 +0800

    finish about page
    Signed-off-by: Kael Li <kaelli@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,20 @@ $( document ).ready(function() {
 	
 	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
 
+
+    //timeline action
+    $('.info_timeline ul li').mouseenter(function(){
+        $(this).addClass('curr');
+        $(this).parent().addClass('focus');
+        var s_id = $(this).attr('id');
+        $('.info_content').addClass('over')
+        $('.'+s_id).addClass('curr');
+    });
+    $('.info_timeline ul li').mouseleave(function(){
+        $(this).removeClass('curr');
+        $(this).parent().removeClass('focus');
+        $('.info_content').removeClass('over')
+        $('.info_content .desc').removeClass('curr');
+    }); 
+
 });
